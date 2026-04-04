5ce904886640b56fedb9f20d947e91ddbf695b44
GreenMoonMan <GreenMoonMan@users.noreply.github.com>
2026-04-04 19:06:03 +0000

    jupytext test

---
+++ notebooks/jupytext_test.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+print("jupytest")
+
+# %% [markdown]
+# # this is markdown
--- test.py
+++
@@ -1,4 +0,0 @@
-import control
-
-print("testing devcontainer")
-
